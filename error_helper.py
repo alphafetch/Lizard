0ffc70d64f5bccf04db0e05804d6001fbf1cddea
theTacocat88 <eliasglick1@gmail.com>
2026-08-16 15:54:41 -0400

    feat(error_helper): add error_helper.py to contain error classes

---
+++ error_helper.py
@@ -0,0 +1,14 @@
+class DeclarationError(Exception):
+    pass
+
+class TypeDeclarationError(Exception):
+    pass
+
+class MalformedNumber(Exception):
+    pass
+
+class UnterminatedString(Exception):
+    pass
+
+class CharacterTooLong(Exception):
+    pass
