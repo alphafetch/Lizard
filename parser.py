9e083d00e688606294b5fff065f2e75e118080d2
theTacocat88 <eliasglick1@gmail.com>
2026-08-16 16:05:12 -0400

    fix(missing semicolon): implement floodgate for missing semicolon after variable declaration

--- parser.py
+++ parser.py
@@ -159,10 +159,12 @@ def parse_var_dec(tokens, i):
                 if not next_char_end:
                     if tokens[i + 1].token in ["TOKEN_NUMBER", "TOKEN_STRING"]:
                         value = Literal(tokens[i + 1].value)
+                        if tokens[i + 2].token != "TOKEN_SEMI":
+                            raise errors.DeclarationError("Declaration must terminate after value.")
                     else:
-                        raise errors.DeclarationError("Value must be of class `Literal`.")
+                        raise     errors.DeclarationError("Value must be of class `Literal`.")
                 else:
-                    raise errors.DeclarationError("Variable assigns with `=` but no value follows.")
+                    raise         errors.DeclarationError("Variable assigns with `=` but no value follows.")
                 
         i += 1
 
