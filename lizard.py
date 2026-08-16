41c1c3f7237c3a66283f04d6905f9c316cf42dbb
theTacocat88 <eliasglick1@gmail.com>
2026-08-16 17:01:11 -0400

    feat(parsing): add parsing logic to lizard.py to test parser

--- lizard.py
+++ lizard.py
@@ -1,8 +1,18 @@
 import tokenizer
+import parser
 
-DEBUG = False
-tokens = tokenizer.tokenize("C:\\Users\\elias\\Documents\\Projects\\Lizard\\Source\\testing\\test2.liz")
+DEBUG_TOKENIZATION = False
+DEBUG_PARSING = True
+tokens = tokenizer.tokenize("C:\\Users\\elias\\Documents\\Projects\\Lizard\\Source\\testing\\test4.liz")
 
-if DEBUG:
+if DEBUG_TOKENIZATION:
     for t in tokens:
         print(t.type, t.token, repr(t.value))
+
+tokens = parser.filter_token_list(tokens)
+
+node, new_index = parser.parse_var_dec(tokens, 0)
+
+if DEBUG_PARSING:
+    print(node, node.type, node.name, node.val)
+    print("new index: ", new_index)
